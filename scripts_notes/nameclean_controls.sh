4e4d1c1b90323bc0b17da9c8a7a8e65dfb5e3575
bolivaraponte <apontebolivar@gmail.com>
2024-02-02 14:17:52 -0600

    Name cleanup for Aponte_8756 sequencing run. Renamed cleaning scripts.

---
+++ scripts_notes/nameclean_controls.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+for file in *.fastq.gz
+do
+ echo "Unziping"
+ gzip -d  "$file"
+done
+
+#Rename the files
+for file in *.fastq
+do
+ echo "Renaming"
+ newname=$(echo $file | cut -d_ -f1,4 | sed 's/-//g').fastq
+ echo "Renaming $file as $newname"
+# mv $file $newname 
+done
+##Script from HPC workshop 2 3/16/2023
+##Updated on 6/7/2023 with troubleshooting with ChatGPT. 
+#Various itterations offered were not exactly what
+# I needed.-BAR
+
