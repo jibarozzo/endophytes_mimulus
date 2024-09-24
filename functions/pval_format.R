1140aa07f90717d0467ef06fae6c9c4d8366ce74
jibarozzo <bolaponte@proton.me>
2024-09-24 15:28:09 -0500

    Incorporating SAV and KGF suggestions.

--- functions/pval_format.R
+++ functions/pval_format.R
@@ -4,9 +4,11 @@ format.p <- function(p, precision = .001) {
     digits <- -log(precision, base = 10)
     p <- formatC(p, format = 'f', digits = digits)
     if (p < .001) {
-        p = paste0('< ', precision)}
+        p = paste0('< ', precision)
+    }
     if (p >= .001) {
-        p = paste0('= ', p)    }
+        p = paste0('= ', p)
+    }
     sub("", "", p)
 }
 ######################################################
